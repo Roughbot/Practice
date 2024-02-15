fa84e71d53b59618a632912f0ed90813d92e70a2
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-02-15 23:38:48 +0530

    15th feb

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4};//[789123456]
+//        int[] arr = {1,2,3,4};
 //      System.out.println(Arrays.toString(reverseTheArray.reverseTheGivenArray(arr)));
-        System.out.println(Arrays.toString(rotateTheArray.rotateTheGivenArray(arr, 3)));
-
+//        System.out.println(Arrays.toString(rotateTheArray.rotateTheGivenArray(arr, 3)));
+        int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        System.out.println(spiralMatrix.printSpiralMatrix(arr));
     }
 }
