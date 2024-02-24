9865e15254f6fed32ae7117c5be5fbad51a5035d
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-02-24 23:02:26 +0530

    24th feb

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 public class Main {
     public static void main(String[] args) {
         int[] arr = {1,1,11,4,3,3,2,5,7,2,6,7,9,1,4};
-        System.out.println(sizeOfSmallestSubArray.sizeOfSmallestSubArray(arr));
+        System.out.println(sizeOfSmallestSubArray.sizeOfSmallestSubArray2(arr));
     }
 }
--- src/sizeOfSmallestSubArray.java
+++ src/sizeOfSmallestSubArray.java
@@ -2,7 +2,7 @@ public class sizeOfSmallestSubArray {
     //Given an array of integer, find the size of the smallest subarray containing both the min and max elements of the array.
     //input:[1,2,3,1,3,4,4]
     //output: 3
-    public static int sizeOfSmallestSubArray(int[] arr){
+    public static int sizeOfSmallestSubArray1(int[] arr){
 
         int max = 0;
         int min = Integer.MAX_VALUE;
@@ -13,7 +13,7 @@ public class sizeOfSmallestSubArray {
             if (j < min) {
                 min = j;
             }
-        }
+        }   
         int answer = Integer.MAX_VALUE;
         for(int i = 0; i < arr.length; i++){
             if(arr[i] == min){
@@ -30,6 +30,35 @@ public class sizeOfSmallestSubArray {
                         answer = Math.min(answer, j-i+1);
                         break;
                     }
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static int sizeOfSmallestSubArray2(int[] arr){
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for(int j : arr){
+            min = Math.min(min, j);
+            max = Math.max(max, j);
+        }
+
+        int answer = Integer.MAX_VALUE;
+        int latest_max = -1;
+        int latest_min = -1;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == min){
+                latest_min = i;
+                if(latest_max != -1){
+                    answer = Math.min(answer, Math.abs(latest_max-latest_min)+1);
+
+                }
+            }
+            if(arr[i] == max){
+                latest_max = i;
+                if(latest_min != -1){
+                    answer = Math.min(answer,Math.abs(latest_min-latest_max)+1);
                 }
             }
         }
