23162e24b42c8cb29a418037fe732649bedfd91a
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-02-25 23:42:06 +0530

    25th feb

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         int[] arr = {1,1,11,4,3,3,2,5,7,2,6,7,9,1,4};
-        System.out.println(sizeOfSmallestSubArray.sizeOfSmallestSubArray2(arr));
+        System.out.println(Arrays.toString(aPlusbEqucPlusd.check4sums(arr)));
+        System.out.println(aPlusbEqucPlusd.check4sums2(arr));
     }
+
 }
